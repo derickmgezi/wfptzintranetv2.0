f887c4d6ce6e14f2cc44a996b8c8fdbab0429783
derickmgezi <derickmgezi@gmail.com>
2018-06-16 16:00:43 +0300

    Hide News Resize Button

--- resources/views/updates.blade.php
+++ resources/views/updates.blade.php
@@ -76,9 +76,9 @@
 
                                 <div class="col-lg-3 col-md-4 text-right">
                                     @if($editors->contains('editor', Auth::id()))
-                                    <a class="btn btn-warning" href="{{ URL::to('/resizenewsthumbnails') }}">
+<!--                                    <a class="btn btn-warning" href="{{ URL::to('/resizenewsthumbnails') }}">
                                        <i class="fa fa-expand" aria-hidden="true"></i>
-                                    </a>
+                                    </a>-->
                                     
                                     <a class="btn btn-success" href="{{URL::to('/add_update')}}" role="button">
                                         <i class="fa fa-plus-square faa-vertical faa-slow animated" aria-hidden="true"></i> Add News Post
