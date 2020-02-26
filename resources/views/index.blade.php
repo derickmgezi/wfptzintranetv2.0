37cede323787216d031dc6a12c4200a27f339ef8
unknown <derickmgezi@gmail.com>
2020-02-26 11:52:50 +0300

    Re-activated OpenID Connect Authentication with temp-domain name

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -93,7 +93,7 @@
             </div>
 
             <!-- New Login Modal -->
-            {{-- <div class="modal fade loginModal modal-position" data-backdrop="static" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+            <div class="modal fade loginModal modal-position" data-backdrop="static" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header bg-primary">
@@ -118,10 +118,10 @@
                         </div>
                     </div>
                 </div>
-            </div> --}}
+            </div>
             
             <!-- Old Login Modal -->
-            <div class="modal fade loginModal modal-position" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+            {{-- <div class="modal fade loginModal modal-position" id="loginModal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
                 <div class="modal-dialog" role="document">
                     <div class="modal-content">
                         <div class="modal-header bg-primary">
@@ -153,7 +153,7 @@
                         </div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
 
         </div>
 
