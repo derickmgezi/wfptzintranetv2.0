d21a75ec0ab6554fa01e4a506794275e3b33ad99
unknown <dericmgezi@gmail.com>
2020-04-05 19:54:58 +0300

    Updates Home Page and Search Page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,7 +20,7 @@
                         <div class="justify-content-start">
                             <h2 class="mr-3">
                                 <span class="small h4">WFP Updates</span>
-                                <span class="smaller font-weight-bold text-primary">recently posted</span>
+                                <span class="smaller font-weight-bold text-primary">posted recently</span>
                             </h2>
                         </div>
 
@@ -74,7 +74,7 @@
                         <div class="justify-content-start">
                             <h2 class="mr-3 mt-3">
                                 <span class="small">Stori Yangu</span>
-                                <span class="smaller font-weight-bold text-primary">recently posted</span>
+                                <span class="smaller font-weight-bold text-primary">posted recently</span>
                             </h2>
                         </div>
 
@@ -126,7 +126,7 @@
                         <div class="justify-content-start">
                             <h2 class="mr-3">
                                 <span class="small">WFP Updates</span>
-                                <span class="smaller font-weight-bold text-primary">recently posted</span>
+                                <span class="smaller font-weight-bold text-primary">posted recently</span>
                             </h2>
                         </div>
 
@@ -213,7 +213,46 @@
                         <div class="justify-content-start">
                             <h2 class="mr-3">
                                 <span class="small">Links</span>
-                                <span class="smaller font-weight-bold text-primary">my top clicks</span>
+                                <span class="smaller font-weight-bold text-primary">WFP favorites</span>
+                            </h2>
+                        </div>
+                        <div class="list-group">
+                            <a class="list-group-item list-group-item-action active mb-1" target="_blank" href="{{URL::to('/external_link/Transport-Request/'.encrypt('https://humanitarianbooking.wfp.org/en/explore/country/tz/?service=UN+Driver+Hub'))}}">
+                                <i class="fa fa-car"aria-hidden="true"></i>&nbsp;Transport Request
+                            </a>
+                            <a class="list-group-item list-group-item-action active mb-1" target="_blank" href="{{URL::to('/external_link/NewGo/'.encrypt('https://newgo.wfp.org'))}}">
+                                <i class="fa fa-sign-out" aria-hidden="true"></i>&nbsp;NewGo
+                            </a>
+                            <a class="list-group-item list-group-item-action active mb-1" target="_blank" href="{{URL::to('/external_link/SelfService/'.encrypt('https://selfservice.go.wfp.org'))}}">
+                                <i class="fa fa-server" aria-hidden="true"></i>&nbsp;Self Service
+                            </a>
+                            <a class="list-group-item list-group-item-action active mb-1" target="_blank" href="{{URL::to('/external_link/WINGSII/'.encrypt('https://mfapps.wfp.org'))}}">
+                                <i class="fa fa-paper-plane" aria-hidden="true"></i>&nbsp;WINGSII
+                            </a>
+                            <a class="list-group-item list-group-item-action active mb-1" target="_blank" href="{{URL::to('/external_link/DSA-Rates/'.encrypt('https://newgo.wfp.org/documents/daily-subsistence-allowance-dsa?country=tanzania-united-rep-of-shilling#block--dsa-rates'))}}">
+                                <i class="fa fa-bar-chart"aria-hidden="true"></i>&nbsp;DSA Rates
+                            </a>
+                            <a class="list-group-item list-group-item-action active mb-1" target="_blank" href="{{URL::to('/external_link/UN-Exchange-Rate/'.encrypt('https://treasury.un.org/operationalrates/OperationalRates.php#T'))}}">
+                                <i class="fa fa-exchange"aria-hidden="true"></i>&nbsp;UN Exchange Rate
+                            </a>
+                            <a class="list-group-item list-group-item-action active mb-1" target="_blank" href="{{URL::to('/external_link/Security-Clearance/'.encrypt('https://trip.dss.un.org/dssweb/WelcometoUNDSS/tabid/105/Default.aspx?returnurl=%2fdssweb%2f'))}}">
+                                <i class="fa fa-shield"aria-hidden="true"></i>&nbsp;Security Clearance
+                            </a>
+                            <a class="list-group-item list-group-item-action active mb-1" target="_blank" href="{{URL::to('/external_link/PACE/'.encrypt('https://pace.wfp.org'))}}">
+                                <i class="fa fa-tachometer" aria-hidden="true"></i>&nbsp;PACE
+                            </a>
+                            <a class="list-group-item list-group-item-action active mb-1" target="_blank" href="{{URL::to('/external_link/WeLearn/'.encrypt('https://welearn.wfp.org'))}}">
+                                <i class="fa fa-leanpub"aria-hidden="true"></i>&nbsp;WeLearn
+                            </a>
+                            <a class="list-group-item list-group-item-action active mb-1" target="_blank" href="{{URL::to('/external_link/Reset-Password/'.encrypt('https://password.go.wfp.org/'))}}">
+                                <i class="fa fa-key"aria-hidden="true"></i>&nbsp;Change Password
+                            </a>
+                        </div>
+
+                        {{-- <div class="justify-content-start">
+                            <h2 class="mr-3">
+                                <span class="small">Links</span>
+                                <span class="smaller font-weight-bold text-primary">my favorites</span>
                             </h2>
                         </div>
                         <div class="list-group">
@@ -221,8 +260,8 @@
                                 @foreach($links as $link)
                                     @foreach($accessed_links as $accessed_link)
                                         @if($accessed_link->action_details == $link->action_details)
-                                        <a class="list-group-item list-group-item-action active mb-1" @if($accessed_link->link_type == "External")target="_blank"@endif href="{{URL::to($accessed_link->link_accessed)}}" class="card-link">
-                                            <?php 
+                                        <a class="list-group-item list-group-item-action active mb-1" @if($accessed_link->link_type == "External")target="_blank"@endif href="{{URL::to($accessed_link->link_accessed)}}">
+                                            <php 
                                                 $strings_to_be_replace = array("Redirected ", "to ", " Page", " displayed", "Accessed ", "");
                                                 $link->action_details = str_replace($strings_to_be_replace, "", $link->action_details);
                                             ?>
@@ -232,24 +271,23 @@
                                     @endforeach
                                 @endforeach
                             @else
-                            <a class="list-group-item list-group-item-action active mb-1" href="{{URL::to('/internaldirectory')}}" class="card-link">
+                            <a class="list-group-item list-group-item-action active mb-1" href="{{URL::to('/internaldirectory')}}">
                                 Telephone Directory
                             </a>
-                            <a class="list-group-item list-group-item-action active mb-1" target="_blank" href="{{URL::to('/external_link/NewGo/'.encrypt('http://go.wfp.org'))}}" class="card-link">
+                            <a class="list-group-item list-group-item-action active mb-1" target="_blank" href="{{URL::to('/external_link/NewGo/'.encrypt('http://go.wfp.org'))}}">
                                 WFPgo
                             </a>
-                            <a class="list-group-item list-group-item-action active mb-1" target="_blank" href="{{URL::to('/external_link/WINGSII/'.encrypt('http://mfapps.wfp.org'))}}" class="card-link">
+                            <a class="list-group-item list-group-item-action active mb-1" target="_blank" href="{{URL::to('/external_link/WINGSII/'.encrypt('http://mfapps.wfp.org'))}}">
                                 WINGSII
                             </a>
-                            <a class="list-group-item list-group-item-action active mb-1" href="{{URL::to('/mediaalerts')}}" class="card-link">
+                            <a class="list-group-item list-group-item-action active mb-1" href="{{URL::to('/mediaalerts')}}">
                                 News Alerts
                             </a>
-                            <a class="list-group-item list-group-item-action active mb-1" href="{{URL::to('/resource#sop')}}" class="card-link">
+                            <a class="list-group-item list-group-item-action active mb-1" href="{{URL::to('/resource#sop')}}">
                                 Resources
                             </a>
-                            
                             @endif
-                        </div>
+                        </div> --}}
                     </div>
                 </div>
 
